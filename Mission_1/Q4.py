a35b50f1e310038e5eaa8db550e7e6eeb384c8c2
IAiAye <holmesian16@gmail.com>
2022-01-18 02:49:49 +0900

    #week1

--- Mission_1/Q4.py
+++ Mission_1/Q4.py
@@ -1,4 +1,4 @@
-dict_first = {"사과": 30, "배": 15, "감": 10, "포도": 10}
+"""
 dict_second = {"사과": 5, "감": 25, "배": 15, "귤": 25}
 
 
@@ -14,3 +14,17 @@ def merge_dict(dict_first, dict_second):
 
 
 print(merge_dict(dict_first, dict_second))
+"""
+from collections import Counter  # Collection 모듈(강의자료 python_datastructure p42 참조)
+
+dict_first = {"사과": 30, "배": 15, "감": 10, "포도": 10}
+dict_second = {"사과": 5, "감": 25, "배": 10, "귤": 25}
+
+
+def merge_dict(dict_first, dict_second):
+    a = Counter({"사과": 30, "배": 15, "감": 10, "포도": 10})
+    b = Counter({"사과": 5, "감": 25, "배": 15, "귤": 25})
+    print(a + b)
+
+
+merge_dict(dict_first, dict_second)
