baa39f7b4f3030db6b9401b2aef7bd5ada9c46b2
IAiAye <holmesian16@gmail.com>
2022-01-25 02:19:18 +0900

    Week2

---
+++ Week_2/closure.py
@@ -0,0 +1,15 @@
+def tag_func(tag, text):
+    tag = tag
+    text = text
+
+    def inner_func():
+        return "<{0}>{1}<{0}>".format(tag, text)
+
+    return inner_func
+
+
+h1_func = tag_func("title", "This is Python Class")
+p_func = tag_func("p", "Data Academy")
+
+print(h1_func)
+print(p_func)
